c3427f5e1c5a6f12f89e7b3d22234e6c69bcf632
Tiorenn <ericorenanleal124@gmail.com>
2026-03-12 18:11:56 -0300

    Product

--- src/main/java/com/cursojava/course/config/Testconfig.java
+++ src/main/java/com/cursojava/course/config/Testconfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.cursojava.course.entities.Category;
 import com.cursojava.course.entities.Order;
+import com.cursojava.course.entities.Product;
 import com.cursojava.course.entities.User;
 import com.cursojava.course.entities.enums.OrderStatus;
 import com.cursojava.course.repositories.CategoryRepository;
 import com.cursojava.course.repositories.OrderRepository;
+import com.cursojava.course.repositories.ProductRepository;
 import com.cursojava.course.repositories.UserRepository;
 
 @Configuration
@@ -29,25 +31,34 @@ public class Testconfig implements CommandLineRunner {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @Override
-    public void run(String... args) throws Exception {  
-        
+    public void run(String... args) throws Exception {
+
         Category cat1 = new Category(null, "Eletronics");
         Category cat2 = new Category(null, "Books");
         Category cat3 = new Category(null, "Computers");
 
-        categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+        Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+        Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+        Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+        Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+        Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
         
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+        productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+
         User u1 = new User(null, "Maria brown", "maria@gmail.com", "999871233", "1010");
         User u2 = new User(null, "AlexGreen", "alex@gmail.com", "999767634", "1234");
-        
+
         Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
-        
-        
-        categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
-        userRepository.saveAll(Arrays.asList(u1,u2));
-        orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+        userRepository.saveAll(Arrays.asList(u1, u2));
+        orderRepository.saveAll(Arrays.asList(o1, o2, o3));
     }
 }
---
+++ src/main/java/com/cursojava/course/repositories/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.cursojava.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.cursojava.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {    
+}

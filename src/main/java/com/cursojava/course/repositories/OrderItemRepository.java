644d2f67371e47f0e6552736bc9b59f4df8739c0
Tiorenn <ericorenanleal124@gmail.com>
2026-03-16 13:52:37 -0300

    User Insert

--- src/main/java/com/cursojava/course/repositories/OrderItemRepository.java
+++ src/main/java/com/cursojava/course/repositories/OrderItemRepository.java
@@ -3,7 +3,8 @@ package com.cursojava.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cursojava.course.entities.OrderItem;
+import com.cursojava.course.pk.OrderItemPk;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem,OrderItemPk> {
     
 }
